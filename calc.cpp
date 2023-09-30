b7597774b820b362a9694bf5feffe91b26deca6e
Mykhailo Halchenko <114739882+MykhailoHalchenko@users.noreply.github.com>
2023-09-30 18:42:30 +0300

    Loading1

---
+++ calc.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float a, b;
+	int op;
+	cout << "Enter Two numbers: " << endl;
+	cin >> a >> b;
+	cout << "Choose Operator: 1. + 2. - 3. * 4. / " << endl;
+	cin >> op;
+
+	switch (op) {
+	case 1:
+		cout << a + b << endl;
+		break;
+	case 2:
+		cout << a - b << endl;
+		break;
+	case 3:
+		cout << a * b << endl;
+		break;
+	case 4:
+		cout << a / b << endl;
+		break;
+	}
+
+	return 0;
+}
